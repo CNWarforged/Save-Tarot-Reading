1aaeee61bfa7dcc38cd73911f65dcf71279ce2dd
CNWarforged <121924263+CNWarforged@users.noreply.github.com>
2025-03-10 20:50:17 -0700

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SaveTarotReading
+﻿using System;
+
+namespace SaveTarotReading
 {
     class SaveTarotReading
     {
@@ -81,20 +83,59 @@
             }
         }
 
-        private static void BasicReading(ref List<string> readingList, string path, TarotList cardList)
+        private static void BasicReading(ref List<string> readingList, string path, TarotList tarotList)
         {
+            int num = Int32.Parse(readingList[0]);
+            string reading = "Here is your tarot reading for " + DateTime.Today.Day.ToString() + ":" + "\n";
+            reading = reading + "The card representing your past is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[1]);
+            reading = reading + "The card representing your present is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[2]);
+            reading = reading + "The card representing your future is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
 
-
-
+            string newFile = path + "Past-Present-Future Tarot Reading " + DateTime.Today.Day + " " + DateTime.Today.Ticks.ToString() + ".txt";
+            WriteFile(reading, newFile);
         }
 
-        private static void CelticReading(ref List<string> readingList, string path, TarotList cardList)
+        private static void CelticReading(ref List<string> readingList, string path, TarotList tarotList)
         {
+            int num = Int32.Parse(readingList[0]);
+            string reading = "Here is your tarot reading for " + DateTime.Today.Day.ToString() + ":" + "\n";
+            reading = reading + "The card representing The Situation is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[1]);
+            reading = reading + "The card representing What Crosses You is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[2]);
+            reading = reading + "The card representing Best That Can Be Achieved is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[3]);
+            reading = reading + "The card representing The Reason For Your Reading is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[4]);
+            reading = reading + "The card representing The Past is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[5]);
+            reading = reading + "The card representing The Near Future is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[6]);
+            reading = reading + "The card representing You is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[7]);
+            reading = reading + "The card representing Your Environment is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[8]);
+            reading = reading + "The card representing your Hopes Or Fears is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
+            num = Int32.Parse(readingList[9]);
+            reading = reading + "The card representing The Outcome is: " + tarotList.CardList[num].Name + "\n";
+            reading = reading + "The description is: " + tarotList.CardList[num].ShortDescription + "\n";
 
-
-
+            string newFile = path + "Celtic Cross Tarot Reading " + DateTime.Today.Day + " " + DateTime.Today.Ticks.ToString() + ".txt";
+            WriteFile(reading, newFile);
         }
     }
-
-
 }
